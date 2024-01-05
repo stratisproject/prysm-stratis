cc995380ba7561b89e4848cdbd14d02fc4b08401
SergeevDmitry <sergeevdms@gmail.com>
2024-01-05 11:35:18 +0200

    Updated Auroria chain id

--- config/params/testnet_auroria_config.go
+++ config/params/testnet_auroria_config.go
@@ -17,8 +17,8 @@ func AuroriaConfig() *BeaconChainConfig {
 	cfg := MainnetConfig().Copy()
 	cfg.ConfigName = AuroriaName
 	cfg.GenesisForkVersion = []byte{10, 0, 10, 20}
-	cfg.DepositChainID = 56201
-	cfg.DepositNetworkID = 56201
+	cfg.DepositChainID = 205205
+	cfg.DepositNetworkID = 205205
 	cfg.AltairForkVersion = []byte{11, 0, 10, 20}
 	cfg.BellatrixForkVersion = []byte{12, 0, 10, 20}
 	cfg.CapellaForkVersion = []byte{13, 0, 10, 20}
